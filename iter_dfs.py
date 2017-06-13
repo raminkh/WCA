5d4e5b6dd0dd5ed7a3b43f9736019da5d84b5a29
raminkh <ramin.khajeh@gmail.com>
2017-06-13 18:03:32 -0400

    or statement was it

--- iter_dfs.py
+++ iter_dfs.py
@@ -5,7 +5,6 @@ import itertools as it
 import time
 from sys import argv
 from networkx.algorithms import isomorphism
-from pandas import *
 
 adjList = {0: set([1,2]), 1: set([0,2,3]), 2: set([0,1,3,4]), 3: set([1,2,4,5]), 4: set([2,3,5]), 5: set([3,4])}
 G = nx.Graph()
@@ -43,33 +42,45 @@ pos = nx.circular_layout(G)
 nx.draw(G,pos)
 nx.draw_networkx_labels(G,pos)
 
+def dfs_paths_onlyadj(graph, start, goal):
+   stack = [(start, [start])]
+   while stack:
+      (vertex, path) = stack.pop()
+      for k in graph[vertex] - set(path):
+         if k == goal or len(path) == 3:
+            yield path + [k]
+         else:
+            stack.append((k, path + [k]))
 def dfs_paths(graph, start, goal):
    stack = [(start, [start])]
    while stack:
       (vertex, path) = stack.pop()
       for k in graph[vertex] - set(path):
          if k == goal:
-            print path + [k]
+            yield path + [k]
          else:
             stack.append((k, path + [k]))
 
+print list(dfs_paths(adjList,0,5))
+print "\n"
+print list(dfs_paths_onlyadj(adjList,0,5))
 
-graph = adjList 
-start = 0
-goal = 5
-stack = [(start, [start])]
-print stack
-while stack:
-   print "stack is ...  ", stack
-   (vertex, path) = stack.pop()
-   print "top of stack is poped, (vertex, path) ...  ", (vertex, path)
-   print "goes to for loop of neighbors"
-   for k in graph[vertex] - set(path):
-      "print neighbor not in path is ...  ", k
-      if k == goal:
-         print "neighbor is goal! path + [k] ...  " , path + [k]
-      else:
-         print "neighbor is not goal! appending to stack (k, path + [k])"
-         stack.append((k, path + [k]))
-   print "\n"
+#graph = adjList 
+#start = 0
+#goal = 5
+#stack = [(start, [start])]
+#print stack
+#while stack:
+#   print "stack is ...  ", stack
+#   (vertex, path) = stack.pop()
+#   print "top of stack is poped, (vertex, path) ...  ", (vertex, path)
+#   print "goes to for loop of neighbors"
+#   for k in graph[vertex] - set(path):
+#      "print neighbor not in path is ...  ", k
+#      if k == goal:
+#         print "neighbor is goal! path + [k] ...  " , path + [k]
+#      else:
+#         print "neighbor is not goal! appending to stack (k, path + [k])"
+#         stack.append((k, path + [k]))
+#   print "\n"
 
